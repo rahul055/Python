33cbb398c35a461fec473a2d78a34a5f183a301d
rahul055 <rahul.joshi@mprocure.in>
2026-03-25 20:11:36 +0530

    improved code as per industry level. used good practices

--- day-1/excercise.py
+++ day-1/excercise.py
@@ -1,30 +1,35 @@
 
 
 def analyze_text(text: str):
-    word_count = len(text.split())
-    summary = " ".join(text.split()[:10])
+    word = text.split()
+    word_count = len(word)
+    text_summary = " ".join(word[:10])
 
 
-    return {
-        "word_count": word_count,
-        "summary": summary
-    }
-
-def reversed_string(text: str):
-    reversed_text = text[::-1]
-
-    return { "reversed_text": reversed_text }
-
-user_input = input("Enter paragraph :")
-
-result = analyze_text(user_input)
-reversed_text = reversed_string(user_input)['reversed_text']
+    return { "word_count": word_count, "text_summary": text_summary }
 
 
-word_count = result["word_count"]
-summary = result['summary']
+def reverse_string(string: str):
+    return string[::-1]
 
 
-print("Reversed :", reversed_text)
-print("Word count :", word_count)
-print("Summary :", summary)
+def main():
+    user_input = input('Enter your text : ')
+
+
+    reversed_string = reverse_string(user_input)
+    result = analyze_text(user_input)
+    word_count = result['word_count']
+    text_summary = result['text_summary']
+
+
+    print("Output : ")
+
+    print('Reversed string : ', reversed_string)
+    print('Words count : ', word_count)
+    print('Summary : ', text_summary + "...")
+
+
+
+if __name__ == "__main__":
+    main()
